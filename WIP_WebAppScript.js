915bb74042f6c1b9ed3c37fd391a56617d30f41b
Tyler <tylerpoore2000@gmail.com>
2019-06-21 22:24:06 -0700

    Stable Rewrite: Fixed Mouse event simulation, Fixed bad call to openBackgroundTab method

--- WIP_WebAppScript.js
+++ WIP_WebAppScript.js
@@ -1,4 +1,6 @@
 alert("This script is now a defunct feature and will no longer be used / utilised.\nRefreshing page automatically! ");
 window.location = 'https://livebacteria.github.io/'
 
-alert("If this page has not automatically refreshed please do so manually by closing and re-navigating back to this site.");
+setTimeout(() => {
+    alert("If this page has not automatically refreshed please do so manually by closing and re-navigating back to this site.");
+},1000);
