feb86f159c17c3339e93a326f52fdbc84bcbf1e7
Poore <poore.tyler@cleanharbors.com>
2020-01-02 15:19:30 -0800

    Updated Code - January Update

--- WebAppScript.js
+++ WebAppScript.js
@@ -1,44 +1,143 @@
-let currVersion = "1.5.1a | Revert Code",
+let currVersion = "1.5.2a | Revert Code",
     devMode = false,
     exit = false,
     dateArray = [],
-    dailyProcess= false,
+    dailyProcess = false,
     dvirLogArray = [],
     startDate,
     employeeArray = [
-        {name: "Flores, Jose",url:"https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1335257756&Date="},
-        {name: "Foster, Vincent",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308558192&Date="},
-        {name: "Jeronimo, Jesus",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308553144&Date="},
-        {name: "Sing, Anil,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308557080&Date="},
-        {name: "Wilson, David,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1353574072&Date="},
-        {name: "Greer, Peter,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308551662&Date="},
-        {name: "Hatt, Burton,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308552158&Date="},
-        {name: "Huff, Daryl,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308559046&Date="},
-        {name: "Johnston, Michael,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308553138&Date="},
-        {name: "Jordon, Timothy,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308556852&Date="},
-        {name: "Longmore, Jessy,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308551288&Date="},
-        {name: "Marc, James",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1346713884&Date="},
-        {name: "Medina, Juan,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308553172&Date="},
-        {name: "Menor Jr., Gene,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1366082712&Date="},
-        {name: "Mora, Pedro,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1369628138&Date="},
-        {name: "Naples, Tina,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308552826&Date="},
-        {name: "Sanchez, Roberto,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1327216822&Date="},
-        {name: "Allen, Rashaan,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308558196&Date="},
-        {name: "Delosreyes, Jose,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1366360486&Date="},
-        {name: "Felder, James,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1330980720&Date="},
-        {name: "Jenkins, Tyrone,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308559004&Date="},
-        {name: "Mcguiness, Jordan",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1349517206&Date="},
-        {name: "Moore, Jason",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1352518174&Date="},
-        {name: "Muhatia, Nixon,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1344934460&Date="},
-        {name: "Pereira, Errol",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1352828404&Date="},
-        {name: "Petrucci, Robert,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308551290&Date="},
-        {name: "Raymundo, Victor,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308556288&Date="},
-        {name: "Romero, Gerrardo,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308551292&Date="},
-        {name: "Santiago, Rosales,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1333448034&Date="},
-        {name: "Shelton, Kenneth,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308555622&Date="},
-        {name: "Soares, Albert,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1330980682&Date="},
-        {name: "Soares, Anthony,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308556830&Date="},
-        {name: "Wilson, Joseph,",url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308556476&Date="}
+        {
+            name: "Flores, Jose",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1335257756&Date="
+        },
+        {
+            name: "Foster, Vincent",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308558192&Date="
+        },
+        {
+            name: "Jeronimo, Jesus",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308553144&Date="
+        },
+        {
+            name: "Sing, Anil,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308557080&Date="
+        },
+        {
+            name: "Wilson, David,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1353574072&Date="
+        },
+        {
+            name: "Greer, Peter,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308551662&Date="
+        },
+        {
+            name: "Hatt, Burton,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308552158&Date="
+        },
+        {
+            name: "Huff, Daryl,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308559046&Date="
+        },
+        {
+            name: "Johnston, Michael,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308553138&Date="
+        },
+        {
+            name: "Jordon, Timothy,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308556852&Date="
+        },
+        {
+            name: "Longmore, Jessy,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308551288&Date="
+        },
+        {
+            name: "Marc, James",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1346713884&Date="
+        },
+        {
+            name: "Medina, Juan,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308553172&Date="
+        },
+        {
+            name: "Menor Jr., Gene,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1366082712&Date="
+        },
+        {
+            name: "Mora, Pedro,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1369628138&Date="
+        },
+        {
+            name: "Naples, Tina,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308552826&Date="
+        },
+        {
+            name: "Sanchez, Roberto,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1327216822&Date="
+        },
+        {
+            name: "Allen, Rashaan,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308558196&Date="
+        },
+        {
+            name: "Delosreyes, Jose,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1366360486&Date="
+        },
+        {
+            name: "Felder, James,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1330980720&Date="
+        },
+        {
+            name: "Jenkins, Tyrone,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308559004&Date="
+        },
+        {
+            name: "Mcguiness, Jordan",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1349517206&Date="
+        },
+        {
+            name: "Moore, Jason",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1352518174&Date="
+        },
+        {
+            name: "Muhatia, Nixon,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1344934460&Date="
+        },
+        {
+            name: "Pereira, Errol",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1352828404&Date="
+        },
+        {
+            name: "Petrucci, Robert,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308551290&Date="
+        },
+        {
+            name: "Raymundo, Victor,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308556288&Date="
+        },
+        {
+            name: "Romero, Gerrardo,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308551292&Date="
+        },
+        {
+            name: "Santiago, Rosales,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1333448034&Date="
+        },
+        {
+            name: "Shelton, Kenneth,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308555622&Date="
+        },
+        {
+            name: "Soares, Albert,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1330980682&Date="
+        },
+        {
+            name: "Soares, Anthony,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308556830&Date="
+        },
+        {
+            name: "Wilson, Joseph,",
+            url: "https://cleanharbors.platform.telogis.com/handler/driver/dailylog.ashx?DriverId=1308556476&Date="
+        }
     ];
 /*
 // Your web app's Firebase configuration
@@ -59,77 +158,60 @@ let URL_GEN = UrlGenerator('Rewritten Versioning'),
     URL = URL_GEN.next().value;
 
 //Url generator increments the date and formats the url correctly for use in telogis connect
-function* UrlGenerator(url, dt = new Date()){
-    //Checks for if daily process is true
-    if($("#listSelector").attr("checked")){
-
-        //Creates new date object from raw date data in the format of YYYY, MM, DD
-        const dateObj = new Date(dateArray[0], dateArray[1], dateArray[2]);
-
-        //Use this code during december then comment out!
-        // let month = dateObj.getMonth();
-        // let formattedMonth = (""+(month+1)).padStart(2,"0") == "00" ? "12" : (""+month).padStart(2,"0");
-
-        while(true){
-            //padStart forces there to be two digits in the YYYY-MM-DD format
-            //date.getMont()+1
-            yield url + dateObj.getFullYear() + (""+dateObj.getMonth()+1).padStart(2,"0") /* formattedMonth */ + (''+dateObj.getDate()).padStart(2,'0') + "&Violations=true&SensorFailures=false";
-            // dateObj.setDate(dateObj.getDate()+1); // Increases a day
-            dateObj = new Date(dateObj.setTime( dateObj.getTime() + 1 * 86400000 ));
-        }
-    }else{
-        while(true){
-            //padStart forces there to be two digits in the YYYY-MM-DD format
-            yield url + dt.getFullYear() + (''+(dt.getMonth()+1)).padStart(2,'0') + (''+dt.getDate()).padStart(2,'0') + "&Violations=true&SensorFailures=false";
-            dt.setDate(dt.getDate()+1); // Increases a day
-        }
+function* UrlGenerator(url, dt = new Date()) {
+    while (true) {
+        //padStart forces there to be two digits in the YYYY-MM-DD format
+        yield url + dt.getFullYear() + ('' + (dt.getMonth() + 1)).padStart(2, '0') + ('' + dt.getDate()).padStart(2, '0') + "&Violations=true&SensorFailures=false";
+        dt.setDate(dt.getDate() + 1); // Increases a day
     }
+}
 }
 
 //Formats parameter date object into html date compatible string (YYYY-MM-DD)
-function dateFormatter(date = new Date()){
+function dateFormatter(date = new Date()) {
     //date.getMont()+1
-    return date.getFullYear() + "-" + (''+(date.getMonth())).padStart(2,'0') + "-" + ('' + date.getDate()).padStart(2,'0');
+    return date.getFullYear() + "-" + ('' + (date.getMonth())).padStart(2, '0') + "-" + ('' + date.getDate()).padStart(2, '0');
 }
 
 //Combines data needed to execute method openNewBackgroundTab and then executes accordingly
-function combineInfo(maxNumberDays){
+function combineInfo(maxNumberDays) {
     const dateObj = new Date(dateArray[0], dateArray[1], dateArray[2]);
-
+    let date = new Date(dateArray[0], dateArray[1], dateArray[2]);
     //Checks if #listSelector (Daily Process checkbox) is checked and that the array employeeArray exists
-    if(employeeArray && $("#listSelector").attr("checked")){
+    if (employeeArray && $("#listSelector").attr("checked")) {
 
         setTimeout(() => {
 
             //for loop executes for as many entries in the employeeArray array
-            for(let x = 0; x < employeeArray.length; x++){
-                URL_GEN = UrlGenerator(employeeArray[x].url, dateObj); //don't forget about the date object issue at hand
+            for (let x = 0; x < employeeArray.length; x++) {
+
+                let targetDate = date;
+                //URL_GEN = UrlGenerator(employeeArray[x].url, dateObj); //don't forget about the date object issue at hand
 
                 //For visuals, we are updating the employee url field on the page
                 $("#employeeID").val(employeeArray[x].url);
-
                 //for loop executes up to the amount of maxNumberDays
-                for(let y = 0; y < maxNumberDays; y++){
-                    // UNCOMMENT AFTER DECEMBER!!
-                    //URL = URL_GEN.next().value;
-
-                    const dateObj = new Date(dateArray[0]-1, dateArray[1], dateArray[2]);
-                    URL = document.querySelector("#employeeID").value + dateObj.getFullYear() + "12" + (''+dateObj.getDate()).padStart(2,'0') + "&Violations=true&SensorFailures=false";
+                for (let y = 0; y < maxNumberDays; y++) {
+                    let formattedDate = targetDate.getFullYear() + ('' + (targetDate.getMonth() + 1)).padStart(2, '0') + ('' + targetDate.getDate()).padStart(2, '0') + "&Violations=true&SensorFailures=false";
+                    URL = `${employeeArray[x].url}+formattedDate`;
                     openBackgroundTab(URL);
+                    targetDate = new Date(date.setTime(date.getTime() + y * 86400000));
                 }
             }
-        },150);
-    }else{
-        for(let z = 0; z < maxNumberDays; z++){
+        }, 150);
+    } else {
+        for (let z = 0; z < maxNumberDays; z++) {
             URL = URL_GEN.next().value;
             openBackgroundTab(URL);
         }
     }
-    if(devMode){console.log("Finished! ");}
+    if (devMode) {
+        console.log("Finished! ");
+    }
 }
 
 //Simulates a mouse control clicking an html link. Opens a new tab with the passed url parameter
-function openBackgroundTab (url){
+function openBackgroundTab(url) {
     let a = document.createElement("a");
     a.href = url;
 
@@ -139,14 +221,14 @@ function openBackgroundTab (url){
 }
 
 //This function toggles the developer mode for the site
-function devModeToggle(){
-    if(!devMode && prompt("Attempting to activate developer mode: \nEnter Credentials: ") == "admin64"){
+function devModeToggle() {
+    if (!devMode && prompt("Attempting to activate developer mode: \nEnter Credentials: ") == "admin64") {
         devMode = true;
 
         alert("Developer mode activated! Please take caution. ");
         $(".footNotation").html(("All Rights Reserved. Released under the MIT license. Copyright Tyler Poore " + dateArray[0] + ", created for general use Clean Harbors© in-house. Logos and Images used are owned, and or managed by Clean Harbors©.<br>DevAppVersion " + currVersion));
         $("#elInput").attr("style", "display: block");
-    }else{
+    } else {
         devMode = false;
 
         alert("Developer mode deactivated! \nTraces of previous code or elements may still exist.\n For safety, please refresh the page.");
@@ -155,41 +237,41 @@ function devModeToggle(){
 }
 
 //This function initiates the entire program when on the DLOG+ page
-function start(load){
+function start(load) {
 
     //Takes date input from html element, splits it by "-" and sets it to an array and then parses all the values to integers
     let maxNumberDays = $("#maxNumberDays").val();
     dateArray = ($("#date-input").val()).split("-");
-    for(let count = 0; count < dateArray.length; count++){
+    for (let count = 0; count < dateArray.length; count++) {
         dateArray[count] = parseInt(dateArray[count]);
     }
     //if(dateArray[2] == "00"){dateArray[2] = 12;}
 
-    if(maxNumberDays > 31 && $("#listSelector").attr("checked") === false){
-        if(confirm("Amount of days entered is high!\nContinue?")){
+    if (maxNumberDays > 31 && $("#listSelector").attr("checked") === false) {
+        if (confirm("Amount of days entered is high!\nContinue?")) {
 
             // overwrite global
             URL_GEN = UrlGenerator(document.querySelector("#employeeID").value, startDate);
             URL = URL_GEN.next().value;
 
-            if(load === 1){
+            if (load === 1) {
                 combineInfo(maxNumberDays);
-            }else{
+            } else {
                 alert("Function not yet added! ");
             }
         }
-    }else if(maxNumberDays > 31 && $("#listSelector").attr("checked")){
+    } else if (maxNumberDays > 31 && $("#listSelector").attr("checked")) {
         alert(`When 'Daily Process' is checked the maximum amount of days can not be higher than 31.\nAmount entered: ${maxNumberDays}`);
-    }else{
+    } else {
         //sets startDate to be equal to the date input field
         startDate = new Date($("#date-input").val());
 
         // overwrite global
         URL_GEN = UrlGenerator(document.querySelector("#employeeID").value, startDate);
         URL = URL_GEN.next().value;
-        if(load === 1){
+        if (load === 1) {
             combineInfo(maxNumberDays);
-        }else{
+        } else {
             alert("Function not yet added! ");
         }
     }
